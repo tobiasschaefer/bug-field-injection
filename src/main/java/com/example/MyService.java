96e3e74865cca151e7dec782599d31ad6352d21d
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2020-07-17 10:08:48 +0200

    Bug: Nullpointer bei field injection durch Service Task mit Expression. Java Class funktioniert, siehe Service2

--- src/main/java/com/example/MyService.java
+++ src/main/java/com/example/MyService.java
@@ -3,6 +3,7 @@ package com.example;
 import io.micronaut.context.annotation.Context;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.impl.el.FixedValue;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -11,12 +12,23 @@ import javax.inject.Singleton;
 @Singleton
 public class MyService {
 
+    public FixedValue fieldvar;
+
     public String hello(DelegateExecution exection) {
-        System.err.println("hello");
+        // TODO: Null-Pointer in the next line. Bug?
+        System.err.println("hello: " + fieldvar.getExpressionText());
         return "hello";
     }
 
     public void print(DelegateExecution exection) {
         System.err.println("result: " + exection.getVariable("myvar"));
+    }
+
+    public FixedValue getFieldvar() {
+        return fieldvar;
+    }
+
+    public void setFieldvar(FixedValue fieldvar) {
+        this.fieldvar = fieldvar;
     }
 }
