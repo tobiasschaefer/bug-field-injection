1858a6f0eda14279547815a64ba2065b7f71cc75
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2020-07-17 09:55:48 +0200

    inject into javaclass

--- src/main/java/com/example/Service2.java
+++ src/main/java/com/example/Service2.java
@@ -2,10 +2,17 @@ package com.example;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.impl.el.FixedValue;
+import org.camunda.bpm.engine.impl.el.JuelExpression;
 
 public class Service2 implements JavaDelegate {
+
+    private JuelExpression myfield;
+
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         System.out.println("3: " + delegateExecution.getVariable("myvar"));
+        System.out.println("myfield: " + myfield.getExpressionText());
+        System.out.println("myfield: " + myfield.getValue(delegateExecution));
     }
 }
